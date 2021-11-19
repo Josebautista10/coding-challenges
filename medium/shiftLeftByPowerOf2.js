4025e20380e265bcabdba0f2d0609a1e89b24474
Josebautista10 <cooldj1020@gmail.com>
2021-11-18 19:56:09 -0700

    completed Shift left by power of two challenge

---
+++ medium/shiftLeftByPowerOf2.js
@@ -0,0 +1,7 @@
+// Write a function that mimics (without the use of <<) the left shift 
+// operator and returns the result from the two given integers.
+
+const shiftToLeft = (x, y) => x * (2** y)
+
+
+console.log(shiftToLeft(10,3))
